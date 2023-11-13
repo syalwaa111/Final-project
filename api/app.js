f1bffc7b37179d2e172b928f07f9144afd6902b6
unknown <asyahnaim18@gmail.com>
2023-11-13 12:35:52 +0700

    fix: perubahan

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRoutes = require("./routes/userRoutes");
+const jobCategoryRoutes = require('./jobCategoryRoutes');
 
 const app = express();
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/jobcategory', jobCategoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
