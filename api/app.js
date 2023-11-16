e8cf3ef802e12a64ed0eaee986c917195d20dd87
unknown <asyahnaim18@gmail.com>
2023-11-16 12:04:13 +0700

    asiyah

--- api/app.js
+++ api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const userRoutes = require("./route/userRoutes");
-const jobCategoryRoutes = require('./jobCategoryRoutes');
+const userRoutes = require("./routes/userRoutes");
+const jobCategoryRoutes = require('./routes/jobCategoryRoutes');
 
 const app = express();
 app.use(express.json());
